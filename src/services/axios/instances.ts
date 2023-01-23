ba18ce2a0c91d1cde5d7be8106f4c64b97b966b8
Isaac Santiago <eoisaacc@gmail.com>
2023-01-22 22:06:33 -0300

    :recycle: refactor: move recognizeApi base values to .env file

--- src/services/axios/instances.ts
+++ src/services/axios/instances.ts
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
+const apiHost = import.meta.env.VITE_RECOGNIZER_API_HOST;
+const apiPort = import.meta.env.VITE_RECOGNIZER_API_PORT;
+
 export const recognizerApi = axios.create({
-  baseURL: 'http://localhost:3000', // development
+  baseURL: `${apiHost}:${apiPort}`,
   headers: {
     'Content-type': 'application/json',
   },
