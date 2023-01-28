872f67dee5eae1353380a0f9e7801f5082e5b2c4
Isaac Santiago <eoisaacc@gmail.com>
2023-01-28 10:49:54 -0300

    :sparkles: feat: create ToggleGroup component

--- src/components/ToggleGroup/index.tsx
+++ src/components/ToggleGroup/index.tsx
@@ -1,1 +1,37 @@
-export const ToggleGroup = () => {};
+import { ErrorMessage, ToggleGroupContainer, ToggleGroupItem } from './styles';
+
+interface ToggleGroupItemProps {
+  label: string | number | JSX.Element;
+  value: string;
+}
+
+interface ToggleGroupProps {
+  items: ToggleGroupItemProps[];
+  value?: string;
+  errorMessage?: string;
+  onValueChange?: (value: string) => void;
+}
+
+export const ToggleGroup = ({
+  items,
+  value,
+  errorMessage,
+  onValueChange,
+}: ToggleGroupProps) => {
+  return (
+    <ToggleGroupContainer
+      type="single"
+      value={value}
+      onValueChange={onValueChange}
+    >
+      {items.map(({ label, value }, index) => {
+        return (
+          <ToggleGroupItem key={`${index}-${label}`} value={value}>
+            {label}
+          </ToggleGroupItem>
+        );
+      })}
+      <ErrorMessage>{errorMessage}</ErrorMessage>
+    </ToggleGroupContainer>
+  );
+};
--- src/components/ToggleGroup/styles.ts
+++ src/components/ToggleGroup/styles.ts
@@ -1,1 +1,32 @@
-import styled from "styled-components";
+import * as ToggleGroup from '@radix-ui/react-toggle-group';
+import styled from 'styled-components';
+
+export const ToggleGroupContainer = styled(ToggleGroup.Root)`
+  display: flex;
+  gap: ${(props) => props.theme.spacing.xs};
+  position: relative;
+  height: min-content;
+`;
+
+export const ToggleGroupItem = styled(ToggleGroup.Item)`
+  padding: ${(props) => props.theme.spacing.xs};
+  font-size: ${(props) => props.theme.text.md};
+  color: ${(props) => props.theme.color['base-800']};
+  text-transform: uppercase;
+  cursor: pointer;
+  border: solid 1px ${(props) => props.theme.color['brand-600']};
+  border-radius: ${(props) => props.theme.rounded.base};
+
+  &[data-state='on'] {
+    color: ${(props) => props.theme.color['base-50']};
+    background-color: ${(props) => props.theme.color['brand-500']};
+  }
+`;
+
+export const ErrorMessage = styled.span`
+  position: absolute;
+  font-size: ${(props) => props.theme.text.sm};
+  line-height: 100%;
+  color: ${(props) => props.theme.color['danger-dark']};
+  top: calc(100% + 2px);
+`;
