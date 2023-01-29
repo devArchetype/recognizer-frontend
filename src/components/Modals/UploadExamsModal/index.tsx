03d3ede8e351e98fca948cd25ce368e4c80e9680
the-riquelme <riquelmedamiaosilva@gmail.com>
2023-01-29 13:57:56 -0300

    :sparkles: feat: add dropzone in modal insert templates

--- src/components/Modals/UploadExamsModal/index.tsx
+++ src/components/Modals/UploadExamsModal/index.tsx
@@ -1,22 +1,63 @@
+import { useState } from 'react';
 import { BaseModal } from '../../base/BaseModal';
-
-import { Uploader } from 'uploader';
-import { UploadDropzone } from 'react-uploader';
+import {
+  Dropzone,
+  FileItem,
+  FileValidated,
+  FullScreenPreview,
+} from '@dropzone-ui/react';
 
 export const UploadExamsModal = () => {
-  const uploader = Uploader({
-    apiKey: 'free',
-  });
-  const options = { multi: true };
+  const [files, setFiles] = useState<FileValidated[]>([]);
+  const [imgSource, setImgSource] = useState<string | undefined>(undefined);
+
+  const updateFiles = (incommingFiles: FileValidated[]) => {
+    setFiles(incommingFiles);
+  };
+
+  const removeFile = (id: number | string | undefined) => {
+    setFiles(files.filter((x) => x.id !== id));
+  };
 
   return (
-    <BaseModal heading="Inserir provas">
-      <UploadDropzone
-        uploader={uploader}
-        options={options}
-        onUpdate={(files) => alert(files.map((x) => x.fileUrl).join('\n'))}
-        width="100%"
-        height="400px"
+    <BaseModal heading="Insira os Gabaritos">
+      <Dropzone
+        style={{ borderWidth: '2px' }}
+        method="POST"
+        textColor="red"
+        onChange={updateFiles}
+        value={files}
+        accept="image/*"
+        color="#4338CA"
+        maxFileSize={3150000}
+        url=""
+        headerClassName={'header-extra-styles'}
+        footer={false}
+        localization="PT-pt"
+      >
+        {files.length <= 0 && (
+          <h3 className="label-dropzone">Drop your files here</h3>
+        )}
+
+        {files.map((file) => (
+          <FileItem
+            {...file}
+            onDelete={removeFile}
+            onSee={(src) => {
+              setImgSource(src);
+            }}
+            key={file.id}
+            info
+            preview
+            hd
+          />
+        ))}
+      </Dropzone>
+
+      <FullScreenPreview
+        imgSource={imgSource}
+        openImage={!!imgSource}
+        onClose={(e: Event) => setImgSource(undefined)}
       />
     </BaseModal>
   );
--- src/components/Modals/UploadExamsModal/styles.ts
+++ src/components/Modals/UploadExamsModal/styles.ts
@@ -1,6 +1,1 @@
 import styled from 'styled-components';
-
-export const DropzoneContainer = styled.div`
-  width: 100%;
-  height: 200px;
-`;
